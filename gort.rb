88ce2ec713ab22e41db401e106c6cec1c819f840
deadprogram <ron@hybridgroup.com>
2017-03-30 08:05:52 +0200

    Update to v0.8.1
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- gort.rb
+++ gort.rb
@@ -1,14 +1,14 @@
 class Gort < Formula
   desc "GORT is a Command Line Toolkit for RobotOps"
   homepage "http://gort.io/"
-  version "0.8.0"
+  version "0.8.1"
 
   if MacOS.prefer_64_bit?
     url "https://s3.amazonaws.com/gort-io/0.8.0/gort_0.8.0_darwin_amd64.zip"
-    sha256 "acc7896f3765fbaea13f2007d87e7914af06d05d681a5ebad4fa22724e560b43"
+    sha256 "be09312d577c477d9720b591044773a8f4f95655c3112a8ab901c5de17fbd4ff"
   else
     url "https://s3.amazonaws.com/gort-io/0.8.0/gort_0.8.0_darwin_386.zip"
-    sha256 "72ea8c8cc8d06c90374f1b503ae8b20cab1991bbd9047c57ddba98b00996a134"
+    sha256 "12e781132a1e6f93fd17e9107af95a4c2534cfcbc4034092aa1d074a61938d50"
   end
 
   bottle :unneeded

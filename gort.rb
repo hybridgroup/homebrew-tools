92534e3111f7d8d839173ec9fa1d6244d1d119b2
deadprogram <ron@hybridgroup.com>
2017-04-06 15:02:50 +0200

    Correct file name to match SHA
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- gort.rb
+++ gort.rb
@@ -4,10 +4,10 @@ class Gort < Formula
   version "0.8.1"
 
   if MacOS.prefer_64_bit?
-    url "https://s3.amazonaws.com/gort-io/0.8.0/gort_0.8.0_darwin_amd64.zip"
+    url "https://s3.amazonaws.com/gort-io/0.8.1/gort_0.8.1_darwin_amd64.zip"
     sha256 "be09312d577c477d9720b591044773a8f4f95655c3112a8ab901c5de17fbd4ff"
   else
-    url "https://s3.amazonaws.com/gort-io/0.8.0/gort_0.8.0_darwin_386.zip"
+    url "https://s3.amazonaws.com/gort-io/0.8.1/gort_0.8.1_darwin_386.zip"
     sha256 "12e781132a1e6f93fd17e9107af95a4c2534cfcbc4034092aa1d074a61938d50"
   end
 
